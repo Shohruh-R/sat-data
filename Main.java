810b5051f30bd33d0db9389f8396d9419e82c509
Shohruh-R <158862425+Shohruh-R@users.noreply.github.com>
2024-05-31 15:28:05 +0000

    found average total score

--- Main.java
+++ Main.java
@@ -25,6 +25,13 @@ public class Main {
     // make sure all data entered the ArrayList
     // System.out.println(totalScore);
     System.out.println(totalScore.size()+" studens took the test.");
+
+    int total = 0;
+    for(int score: totalScore){
+      total +=score;
+    }
+    double average = (double)total/totalScore.size();
+    System.out.print(average);
     
   }
 
