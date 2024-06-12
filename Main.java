d41fa9b26d32b71a4275f442eacd97d911b24d44
Shohruh-R <158862425+Shohruh-R@users.noreply.github.com>
2024-06-11 21:17:38 -0400

    Updated Main.java

--- Main.java
+++ Main.java
@@ -7,32 +7,38 @@ public class Main {
 
     // create data structure to store all total scores
     ArrayList<Integer> totalScore = new ArrayList<Integer>();
-    
-    try{
+
+    try {
       Scanner s = new Scanner(new File("total.txt"));
 
-      while(s.hasNext()){
+      while (s.hasNext()) {
         int score = Integer.parseInt(s.nextLine());
         totalScore.add(score);
       }
       s.close();
-    }
-
-    catch(Exception e){
+    } catch (Exception e) {
       System.out.println("file not found");
     }
 
     // make sure all data entered the ArrayList
-    // System.out.println(totalScore);
-    System.out.println(totalScore.size()+" studens took the test.");
+    System.out.println(totalScore.size() + " students took the test.");
 
     int total = 0;
-    for(int score: totalScore){
-      total +=score;
-    }
-    double average = (double)total/totalScore.size();
-    System.out.print(average);
-    
-  }
+    int count1000OrGreater = 0; // Counter for students who scored 1000 or greater
+    int countLowerThan1000 = 0; // Counter for students who scored lower than 1000
 
+    for (int score : totalScore) {
+      total += score;
+      if (score >= 1000) {
+        count1000OrGreater++;
+      } else {
+        countLowerThan1000++;
+      }
+    }
+
+    double average = (double) total / totalScore.size();
+    System.out.println("Average Score: " + average);
+    System.out.println("Number of students who scored 1000 or greater: " + count1000OrGreater);
+    System.out.println("Number of students who scored lower than 1000: " + countLowerThan1000);
+  }
 }
